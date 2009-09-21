da7093c08831651fe09be6a75ab87b1e542b94b6
Ian Blenke <ian@blenke.com>
2009-09-20 23:00:44 -0400

    Still not generating a collection, but the errors have abated for the time being

--- lib/empty_root_storage_resource.rb
+++ lib/empty_root_storage_resource.rb
@@ -2,50 +2,60 @@ class EmptyRootStorageResource
   include WebDavResource 
 
   def href
+    @logger.info("EmptyRootStorageResource.href => '#{@path}'")
     @path
   end
 
   def initialize(path)
+    @logger = AppEngine::Logger.new
+    @logger.info("EmptyRootStorageResource.initialize(#{path})")
     @path = path
   end
     
   def properties
+    @logger.info("EmptyRootStorageResource.properties => []")
     Array.new
   end
   
   def delete!
-
+    @logger.info("EmptyRootStorageResource.delete!")
   end
   
   def move! (dest_path, depth)
-    
+    @logger.info("EmptyRootStorageResource.move!(#{dest_path},#{depth})")
   end
     
   def copy! (dest_path, depth)
-    
+    @logger.info("EmptyRootStorageResource.copy!(#{dest_path},#{depth})")
   end
   
   def status
+    @logger.info("EmptyRootStorageResource.status => 200 OK")
     gen_status(200, "OK").to_s
   end
   
   def collection?
+    @logger.info("EmptyRootStorageResource.collection? => true")
     true
   end
   
   def children
+    @logger.info("EmptyRootStorageResource.children")
     return []
   end
 
   def get_displayname
+    @logger.info("EmptyRootStorageResource.get_displayname")
     ""
   end
 
   def get_href
+    @logger.info("EmptyRootStorageResource.get_href")
     self.href.gsub(/\+/, '%20') unless self.href.nil?
   end
 
   def get_properties
+    @logger.info("EmptyRootStorageResource.get_properties")
     hsh = {}
     self.properties.each do|meth|
       if self.respond_to?('get_'+meth.to_s)
@@ -59,6 +69,7 @@ class EmptyRootStorageResource
 
   protected
    def gen_element(elem, text = nil, attrib = {})
+     @logger.info("EmptyRootStorageResource.gen_element(#{elem.to_json},#{text},#{attrib})")
      e = REXML::Element.new elem
      text and e.text = text
      attrib.each {|k, v| e.attributes[k] = v }
@@ -66,6 +77,7 @@ class EmptyRootStorageResource
    end
 
    def gen_status(status_code, reason_phrase)
+     @logger.info("EmptyRootStorageResource.gen_status(#{status_code},#{reason_phrase})")
        "HTTP/1.1 #{status_code} #{reason_phrase}"
    end
 end
